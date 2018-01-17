ec2edc949244f9e41131c3f32f51e05b14e561bc
ba030118 <ba030118@gmail.com>
2018-01-17 09:47:22 +0100

    17012018
    views

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('currentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-	res.send({
-		name: 'Andrew',
-		likes: [
-			'Biking',
-			'Cities'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Welcome Page',
+		//currentYear: new Date().getFullYear(),
+		welcomeMessage: 'Hello, visitor!'
 	});
+	// res.send({
+	// 	name: 'Andrew',
+	// 	likes: [
+	// 		'Biking',
+	// 		'Cities'
+	// 	]
+	// });
 });
 
 app.get('/about', (req, res) => {
-	res.send('About Page');
+	//res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
 });
 
 app.get('/bad', (req, res) => {
@@ -22,4 +43,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('server is running on port 3000');
+});
